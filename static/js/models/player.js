98dc155f98b31a583ef5549488a4b4e2985df797
Rowan Crawford <wombleton@gmail.com>
2013-02-20 11:04:18 +1300

    validation fn

--- static/js/models/player.js
+++ static/js/models/player.js
@@ -1,11 +1,15 @@
 define([
     'backbone',
+    'underscore.string',
     'backbone-localStorage'
-], function(Backbone, ls) {
+], function(Backbone, _s, ls) {
     return Backbone.Model.extend({
         parse: function(response) {
             return response;
         },
-        localStorage: new Backbone.LocalStorage('Player')
+        localStorage: new Backbone.LocalStorage('Player'),
+        validate: function() {
+            return !!_s.trim(this.get('name'));
+        }
     });
 });
