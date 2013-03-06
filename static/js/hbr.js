fa17f2f94c3d7d85b7920d02b62476a9a16fb76f
Rowan Crawford <wombleton@gmail.com>
2013-03-07 00:30:07 +1300

    Add enable/disable fns

--- static/js/hbr.js
+++ static/js/hbr.js
@@ -2,4 +2,18 @@ require([
     'jquery',
     '/js/app.js',
     'text' // to allow for loading text for templates; never used directly
-], function($) {});
+], function($) {
+    $.fn.enable = function(flag) {
+        if (arguments.length === 0) {
+            this.removeAttr('disabled');
+        } else if (flag) {
+            this.enable();
+        } else {
+            this.disable();
+        }
+    };
+
+    $.fn.disable = function() {
+        this.attr('disabled', 'disabled');
+    };
+});
