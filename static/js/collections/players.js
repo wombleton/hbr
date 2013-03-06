4ca79a1e9f07e4910747babbff33004a6ff7f78d
Rowan Crawford <wombleton@gmail.com>
2013-03-07 00:29:39 +1300

    Bulk up functionality of players/player

--- static/js/collections/players.js
+++ static/js/collections/players.js
@@ -5,6 +5,46 @@ define([
 ], function(Backbone, BackboneLS, Player) {
     return Backbone.Collection.extend({
         localStorage: new Backbone.LocalStorage('Player'),
-        model: Player
+        model: Player,
+        indexOfType: function(type) {
+            var model = this.getByType(type);
+
+            return this.models.indexOf(model);
+        },
+        getByType: function(type) {
+            return this.detect(function(model) {
+                return model.get('type') === type;
+            });
+
+        },
+        setType: function(index, type) {
+            // clear old type
+            var oldType = this.at(this.indexOfType(type)),
+                model;
+
+            if (oldType) {
+                oldType.unset('type');
+                oldType.unset('words');
+
+                oldType.save();
+            }
+
+            if (index < 0) {
+                index = this.length - 1;
+            } else if (index >= this.length) {
+                index = 0;
+            }
+
+            var model = this.at(index);
+
+            model.set({
+                type: type,
+                words: 0
+            });
+
+            model.save();
+
+            return model;
+        }
     });
 });
--- static/js/models/player.js
+++ static/js/models/player.js
@@ -4,15 +4,18 @@ define([
     'underscore.string'
 ], function(Backbone, BackboneLS, _s) {
     return Backbone.Model.extend({
-        parse: function(response) {
-            return response;
-        },
         localStorage: new Backbone.LocalStorage('Player'),
         validate: function() {
             if (!_s.trim(this.get('name'))) {
                 return 'name required';
             }
             return null;
+        },
+        setType: function(type) {
+            this.set({
+                words: 0,
+                type: type
+            });
         }
     });
 });
